2c49f991325a686eb8679a9c62acd47ecfd26484
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2025-10-15 18:49:29 +0500

    vue app is built and ready for being consumed by host

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    federation({
+      name: "vueApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.vue",
+      },
+      shared: ["vue"],
+    }),
+  ],
   server: {
     port: 3001,
   },
+  build: "esnext",
 });
